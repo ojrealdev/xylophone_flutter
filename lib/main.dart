65218892349ff84a115d674ac1987e306b2bce65
ojrealdev <stephenojwang11@gmail.com>
2022-02-04 14:41:47 +0300

    refactored building keys into function

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,28 @@ import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-void playSound(int keyNumber) {
-  final player = AudioCache();
-  player.play('note$keyNumber.wav');
-}
-
 class XylophoneApp extends StatelessWidget {
+  void playSound(int keyNumber) {
+    final player = AudioCache();
+    player.play('note$keyNumber.wav');
+  }
+
+// method to print a key
+  buildKey(soundNumber, color) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: () => playSound(soundNumber),
+          child: Text(
+            'Sound $soundNumber',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,89 +34,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Container(
-                  color: Colors.red,
-                  child: TextButton(
-                    onPressed: () => playSound(1),
-                    child: Text(
-                      'Sound 1',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.yellowAccent,
-                  child: TextButton(
-                    onPressed: () => playSound(2),
-                    child: Text(
-                      'Sound 2',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.orangeAccent,
-                  child: TextButton(
-                    onPressed: () => playSound(3),
-                    child: Text(
-                      'Sound 3',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: Container(
-                color: Colors.green,
-                child: TextButton(
-                  onPressed: () => playSound(4),
-                  child: Text(
-                    'Sound 4',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )),
-              Expanded(
-                child: Container(
-                  color: Colors.greenAccent,
-                  child: TextButton(
-                    onPressed: () => playSound(5),
-                    child: Text(
-                      'Sound 5',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.blue,
-                  child: TextButton(
-                    onPressed: () => playSound(6),
-                    child: Text(
-                      'Sound 6',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.pinkAccent,
-                  child: TextButton(
-                    onPressed: () => playSound(7),
-                    child: Text(
-                      'Sound 7',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
+              buildKey(1, Colors.blueAccent),
+              buildKey(2, Colors.orangeAccent),
+              buildKey(3, Colors.yellowAccent),
+              buildKey(4, Colors.deepOrange),
+              buildKey(5, Colors.cyan),
+              buildKey(6, Colors.lightGreen),
+              buildKey(7, Colors.amberAccent),
             ],
           ),
         ),
