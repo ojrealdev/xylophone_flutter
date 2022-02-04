2362dd3a2a9ab61d8c5899c4cf5eff0092fb8ec1
ojrealdev <stephenojwang11@gmail.com>
2022-02-04 14:04:15 +0300

    used expanded widget to make my keyboard container fit the screen as designed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
-void playSound(keyNumber) {
+void playSound(int keyNumber) {
   final player = AudioCache();
   player.play('note$keyNumber.wav');
 }
@@ -13,44 +13,49 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizedBox(height: 5),
-              Container(
-                color: Colors.red,
-                child: TextButton(
-                  onPressed: () => playSound(1),
-                  child: Text(
-                    'Sound 1',
-                    style: TextStyle(color: Colors.white),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    onPressed: () => playSound(1),
+                    child: Text(
+                      'Sound 1',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 5),
-              Container(
-                color: Colors.yellowAccent,
-                child: TextButton(
-                  onPressed: () => playSound(2),
-                  child: Text(
-                    'Sound 2',
-                    style: TextStyle(color: Colors.white),
+              Expanded(
+                child: Container(
+                  color: Colors.yellowAccent,
+                  child: TextButton(
+                    onPressed: () => playSound(2),
+                    child: Text(
+                      'Sound 2',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 5),
-              Container(
-                color: Colors.orangeAccent,
-                child: TextButton(
-                  onPressed: () => playSound(3),
-                  child: Text(
-                    'Sound 3',
-                    style: TextStyle(color: Colors.white),
+              Expanded(
+                child: Container(
+                  color: Colors.orangeAccent,
+                  child: TextButton(
+                    onPressed: () => playSound(3),
+                    child: Text(
+                      'Sound 3',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 5),
-              Container(
+              Expanded(
+                  child: Container(
                 color: Colors.green,
                 child: TextButton(
                   onPressed: () => playSound(4),
@@ -59,37 +64,40 @@ class XylophoneApp extends StatelessWidget {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ),
-              SizedBox(height: 5),
-              Container(
-                color: Colors.greenAccent,
-                child: TextButton(
-                  onPressed: () => playSound(5),
-                  child: Text(
-                    'Sound 5',
-                    style: TextStyle(color: Colors.white),
+              )),
+              Expanded(
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: TextButton(
+                    onPressed: () => playSound(5),
+                    child: Text(
+                      'Sound 5',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 5),
-              Container(
-                color: Colors.blue,
-                child: TextButton(
-                  onPressed: () => playSound(6),
-                  child: Text(
-                    'Sound 6',
-                    style: TextStyle(color: Colors.white),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    onPressed: () => playSound(6),
+                    child: Text(
+                      'Sound 6',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-              SizedBox(height: 5),
-              Container(
-                color: Colors.pinkAccent,
-                child: TextButton(
-                  onPressed: () => playSound(7),
-                  child: Text(
-                    'Sound 7',
-                    style: TextStyle(color: Colors.white),
+              Expanded(
+                child: Container(
+                  color: Colors.pinkAccent,
+                  child: TextButton(
+                    onPressed: () => playSound(7),
+                    child: Text(
+                      'Sound 7',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
